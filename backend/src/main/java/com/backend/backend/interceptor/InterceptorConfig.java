938882cc1e017e3fef579e798c786244add95dc2
GO-Lear <67890933+GO-Lear@users.noreply.github.com>
2020-07-24 11:08:25 +0800

    Add files via upload

---
+++ backend/src/main/java/com/backend/backend/interceptor/InterceptorConfig.java
@@ -0,0 +1,36 @@
+package com.backend.backend.interceptor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+/**
+ * @ClassName InterceptorConfig
+ * @Description TODO
+ * @Author thunderBoy
+ * @Date 2019/11/7 20:11
+ */
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+}
