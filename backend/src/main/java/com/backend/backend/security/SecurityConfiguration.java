eecd3c070649be4cbded8322b7ae99bda07f1c4b
StevenLiu99 <49866763+StevenLiu99@users.noreply.github.com>
2020-09-07 21:14:33 +0800

    1

--- backend/src/main/java/com/backend/backend/security/SecurityConfiguration.java
+++ backend/src/main/java/com/backend/backend/security/SecurityConfiguration.java
@@ -25,13 +25,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.formLogin()                    //  定义当需要用户登录时候，转到的登录页面。
-                .loginPage("/login.html")           // 设置登录页面
-                .loginProcessingUrl("/user/login")  // 自定义的登录接口
-                .and()
-                .authorizeRequests()        // 定义哪些URL需要被保护、哪些不需要被保护
-                .antMatchers("/login.html").permitAll() ;  // 设置所有人都可以访问登录页面
-
+        http.httpBasic();
         http.authorizeRequests()
                 .antMatchers("/search").permitAll()
                 .antMatchers("/signup").permitAll()
